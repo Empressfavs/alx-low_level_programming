19313db6ea07ec08b1b36196c33e21e65543d5db
Empressfavs <favouradaavwopiabro@gmail.com>
2023-10-07 06:29:49 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,16 +5,14 @@
  */
 int main(void)
 {
-	char a;
-	int b;
-	long int c;
-	long long int d;
-	float f;
-
-	printf("Size of a char: %lu byte(s)\n", (unsigned long) sizeof(a));
-	printf("size of an int: %lu byte(s)\n", (unsigned long) sizeof(b));
-	printf("size of a long int: %lu byte(s)\n", (unsigned long) sizeof(c));
-	printf("size of a long long int: %lu byte(s)\n", (unsigned long) sizeof(d));
-	printf("size of a float: %lu byte(s)\n", (unsigned long) sizeof(f));
-	return (0);
+printf("Size of char: %lu byte(s)\n", sizeof(char));
+printf("Size of int: %lu byte(s)\n", sizeof(int));
+printf("Size of short: %lu byte(s)\n", sizeof(short));
+printf("Size of long: %lu byte(s)\n", sizeof(long));
+printf("Size of long long: %lu byte(s)\n", sizeof(long long));
+printf("Size of float: %lu byte(s)\n", sizeof(float));
+printf("Size of double: %lu byte(s)\n", sizeof(double));
+printf("Size of long double: %lu byte(s)\n", sizeof(long));
+return (0);
 }
+
