bab03eb832e2399fc6f68313011c53ecb951f9e5
Empressfavs <favouradaavwopiabro@gmail.com>
2023-10-08 10:22:24 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - A program that prints the alphabet in both lowwer case and upper case
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char c;
+	char d;
+
+	c = 'a';
+	d = 'A';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	while (d <= 'Z')
+	{
+		putchar(d);
+		d++;
+	}
+	putchar('\n');
+	return (0);
+}
