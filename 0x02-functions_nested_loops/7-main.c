f0f574ae7cc435ba571be3de67e9b28bb6333444
Empressfavs <favouradaavwopiabro@gmail.com>
2023-10-11 04:52:24 -0700

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-main.c
+++ 0x02-functions_nested_loops/7-main.c
@@ -1,16 +1,3 @@
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-int r;
+#include "main.h"
 
-print_last_digit(98);
-print_last_digit(0);
-r = print_last_digit(-1024);
-_putchar('0' + r);
-_putchar('\n');
-return (0);
-}
+/**                                                                                 * main - check the code                                                            *                                                                                  * Return: Always 0.                                                                */                                                                                int main(void)                                                                     {                                                                                  int r;                                                                                                                                                                print_last_digit(98);                                                              print_last_digit(0);                                                               r = print_last_digit(-1024);                                                       _putchar('0' + r);                                                                 _putchar('\n');                                                                    return (0);                                                                        }           
