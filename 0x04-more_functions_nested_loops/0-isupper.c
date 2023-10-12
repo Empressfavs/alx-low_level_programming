0bd537276c52541c4bd4b1b778310c037dee3bc8
Empressfavs <favouradaavwopiabro@gmail.com>
2023-10-12 08:50:55 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _isupper - check main
+ * @c: An input character
+ * Description: Writes a function character
+ * Return: Return 1 if it is uppercase or 0 if it is otherwisw
+ */
+int _isupper(int c)
+{
+	char upper;
+	int result;
+
+	result = 0;
+		for (upper = 'A'; upper <= 'Z'; upper++)
+		{
+			if (c == upper)
+			{
+				upper = 1;
+			}
+		}
+		return (result);
+}
