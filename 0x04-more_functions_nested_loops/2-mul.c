2498704a685def292314785032045c807b176ac7
Empressfavs <favouradaavwopiabro@gmail.com>
2023-10-12 09:45:19 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * mul - check main
+ * @a: the number to print
+ * @b: the number to print
+ * Return: Always 0 (Success)
+ */
+int mul(int a, int b)
+{
+	int n;
+
+	n = (a * b);
+	return (n);
+}
