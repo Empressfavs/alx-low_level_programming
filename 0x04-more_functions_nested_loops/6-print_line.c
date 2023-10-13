93d4f0865d1677565a92e16fd347430ece87d6c1
Empressfavs <favouradaavwopiabro@gmail.com>
2023-10-13 08:03:30 -0700

    5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_line -  A function that prits a straight line
+ * @n: number of times character should be printed
+ * Return: if n is 0 or less,, the function should only print new line 
+ */
+void print_line(int n)
+{
+	int a = 1;
+	
+	if (a >= 1)
+	{
+		_putchar("______");
+			a == n;
+	}
+	else
+	{
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
