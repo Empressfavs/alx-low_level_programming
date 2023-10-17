ad4503205306044ebca815b26f93939244969332
Empressfavs <favouradaavwopiabro@gmail.com>
2023-10-17 08:06:11 -0700

    3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
- * _puts - prints a string followed by a new line
- * @str - string to print
+ * _puts -  a function that prints a string, followed by a new line
+ * @str: string to print
  * Return: void
  */
 void _puts(char *str)
