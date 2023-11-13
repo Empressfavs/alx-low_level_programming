6e91896c9f60fe8b6b0f6a7048620b00b7fc9114
Empressfavs <favouradaavwopiabro@gmail.com>
2023-11-13 06:14:14 -0800

    2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -8,13 +8,14 @@
  */
 void print_dog(struct dog *d)
 {
+	if (d != NULL)
+	{
 		if (d->name == NULL)
-			printf("(nil)");
+			printf("Name: (nil)\n");
 		printf("Name: %s\n", d->name);
 		printf("Age: %.6f\n", (*d).age);
 		if (d->owner == NULL)
-			printf("(nil)");
+			printf("Owner: (nil)\n");
 		printf("Owner: %s\n", d->owner);
-		if (d == NULL)
-			printf(" ");
+	}
 }
